068afe3a5b8d1682704d66b77b80c0074c0b61b7
Simon Kesteloot <kurze@kurze.no-ip.info>
2012-12-16 20:53:52 +0100

    ajout aff_membres, modif nom ma_cmd -> c, modif parse_args

--- cmd.c
+++ cmd.c
@@ -1,29 +1,29 @@
 #include <string.h>
 #include "cmd.h"
 
-void parse_membres(char *chaine,cmd *ma_cmd){
+void parse_membres(char *chaine,cmd *c){
 	unsigned int i=0;
 	char * cTmp;
 
-	ma_cmd->cmd_initial = chaine;
+	c->cmd_initial = chaine;
 
-	ma_cmd->cmd_membres = (char **) malloc(sizeof(char *)*1);
-	if(ma_cmd->cmd_membres==NULL){
+	c->cmd_membres = (char **) malloc(sizeof(char *)*1);
+	if(c->cmd_membres==NULL){
 		perror("allocation raté // parse_membres");
 		exit(EXIT_FAILURE);
 	}
 
-	cTmp = strtok(ma_cmd->cmd_initial, "|");
+	cTmp = strtok(c->cmd_initial, "|");
 	while( cTmp != NULL){
 		// réalocation du tableau
-		ma_cmd->cmd_membres = (char **) realloc(ma_cmd->cmd_membres, sizeof(char *)*(i+1));
-		if(ma_cmd->cmd_membres==NULL){
+		c->cmd_membres = (char **) realloc(c->cmd_membres, sizeof(char *)*(i+1));
+		if(c->cmd_membres==NULL){
 			perror("réallocation raté // parse_membres");
 			exit(EXIT_FAILURE);
 		}
 
 		// affectation
-		ma_cmd->cmd_membres[i] = cTmp;
+		c->cmd_membres[i] = cTmp;
 
 		//extraction du membres suivants
 		cTmp = strtok(NULL, "|");
@@ -31,29 +31,30 @@ void parse_membres(char *chaine,cmd *ma_cmd){
 		i++;
 
 	}
-	ma_cmd->nb_membres = i;
-// 	printf(" ma_cmd->nb_membres = %d", ma_cmd->nb_membres);
+	c->nb_membres = i;
+// 	printf(" c->nb_membres = %d", c->nb_membres);
 }
 
-void aff_membres(cmd *ma_cmd){
+void aff_membres(cmd *c){
 	unsigned int i=0;
-	printf("nombre de membres : %d\n", ma_cmd->nb_membres);
-	while(i < (ma_cmd->nb_membres)){
-		printf("membres n°%d : %s\n", i, ma_cmd->cmd_membres[i]);
+	printf("nombre de membres : %d\n", c->nb_membres);
+	while(i < (c->nb_membres)){
+		printf("membres n°%d : %s\n", i, c->cmd_membres[i]);
 		i++;
 	}
+	printf("\n");
 }
 
-void free_membres(cmd *ma_cmd){
-	if(ma_cmd->cmd_membres!=NULL){
+void free_membres(cmd *c){
+	if(c->cmd_membres!=NULL){
 		//ne supprimer que le tableau et pas chaque case, car crée erreur sinon (et passe pas valgrind)
-		free(ma_cmd->cmd_membres);
-		ma_cmd->nb_membres = 0;
+		free(c->cmd_membres);
+		c->nb_membres = 0;
 	}
 }
 
 void parse_args(cmd *c){
-	unsigned int i, j;
+	unsigned int i, j, redirection;
 	char * cTmp;
 
 	// allocation de la première dimension du tableau
@@ -63,7 +64,14 @@ void parse_args(cmd *c){
 		exit(EXIT_FAILURE);
 	}
 
+	c->nb_args_membres = (unsigned int *) malloc(sizeof(unsigned int)*c->nb_membres);
+	if(c->nb_args_membres==NULL){
+		perror("allocation raté // parse_args");
+		exit(EXIT_FAILURE);
+	}
+
 	for(i=0; i < c->nb_membres; i++){
+		j=0;
 
 		c->cmd_args[i] = (char **) malloc(sizeof(char *)*1);
 		if(c->cmd_args[i]==NULL){
@@ -71,15 +79,11 @@ void parse_args(cmd *c){
 			exit(EXIT_FAILURE);
 		}
 
-		c->nb_args_membres = (unsigned int *) malloc(sizeof(unsigned int)*i);
-		if(c->cmd_args[i]==NULL){
-			perror("allocation raté // parse_args");
-			exit(EXIT_FAILURE);
-		}
 
 		cTmp = strtok(c->cmd_membres[i], " ");
 
-		while( cTmp != NULL){
+		redirection = 0;
+		while( cTmp != NULL && !redirection){
 			// réalocation du tableau
 			c->cmd_args[i] = (char **) realloc(c->cmd_args[i], sizeof(char *)*(j+1));
 			if(c->cmd_args[i]==NULL){
@@ -89,14 +93,15 @@ void parse_args(cmd *c){
 
 			// affectation
 			// test si l'argument est une redirection (si oui ->  enregistre NULL)
-			if(index(cTmp, (int) ">") != NULL && index(cTmp, (int) "<") != NULL){
+			if(index(cTmp, '>') == NULL && index(cTmp, '<') == NULL){
 				c->cmd_args[i][j] = cTmp;
 			}else{
 				c->cmd_args[i][j] = NULL;
+				redirection = 1;
 			}
 
 			//extraction du membres suivants
-			cTmp = strtok(NULL, "|");
+			cTmp = strtok(NULL, " ");
 
 			j++;
 		}
@@ -105,5 +110,18 @@ void parse_args(cmd *c){
 	}
 }
 
-void aff_args(cmd *c);
+void aff_args(cmd *c){
+	unsigned int nbMembres=0, nbArgs=0;
+	while(nbMembres< c->nb_membres){
+		nbArgs=0;
+		printf("nombre d'arguments pour le membres %d: %d\n",nbMembres,  c->nb_args_membres[nbMembres]);
+		while(nbArgs < c->nb_args_membres[nbMembres]){
+			printf("\targument n°%d du menbres n°%d: %s\n", nbArgs, nbMembres, c->cmd_args[nbMembres][nbArgs]);
+			nbArgs++;
+		}
+		nbMembres++;
+	}
+	printf("\n");
+}
+
 void free_args(cmd *c);
