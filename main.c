5764cbfed98e68d832389adf2a845dbcad488a54
Simon Kesteloot <kurze@kurze.no-ip.info>
2012-12-21 15:22:16 +0100

    allocation dynamique cmd et factorisation des #ifdef DEBUG_FLAG // main()

--- main.c
+++ main.c
@@ -11,17 +11,18 @@ int main(void)
 {
 	//..........
 	int ret = MYSHELL_CMD_OK;
-	cmd mycmd;
+	cmd * mycmd;
 	char* readlineptr;
 	struct passwd* infos;
 	char str[1024];
 	char hostname[256];
 	char workingdirectory[256];
 
-	initCMD(&mycmd);
-
 	while(ret != MYSHELL_FCT_EXIT)
 	{
+		mycmd = (cmd *) malloc(sizeof(cmd) * 1);
+		initCMD(mycmd);
+
 		infos=getpwuid(getuid());
 		gethostname(hostname, 256);
 		getcwd(workingdirectory, 256);
@@ -30,30 +31,23 @@ int main(void)
 		readlineptr = readline(str);
 
 		//..........
-		parse_membres(readlineptr, &mycmd);
+		parse_membres(readlineptr, mycmd);
+		parse_args(mycmd);
+		parse_redirect(mycmd);
 
 #ifdef DEBUG_FLAG
-		aff_membres(&mycmd);
-#endif
-
-		parse_args(&mycmd);
-
-#ifdef DEBUG_FLAG
-		aff_args(&mycmd);
-#endif
-
-		parse_redirect(&mycmd);
-
-#ifdef DEBUG_FLAG
-		aff_redirect(&mycmd);
+		aff_membres(mycmd);
+		aff_args(mycmd);
+		aff_redirect(mycmd);
 #endif
 		//..........exec_cmd(&mycmd);
 		//..........
 
 		free(readlineptr);
-		free_redirect(&mycmd);
-		free_args(&mycmd);
-		free_membres(&mycmd);
+		free_redirect(mycmd);
+		free_args(mycmd);
+		free_membres(mycmd);
+		free(mycmd);
 		//..........
 
 	}
