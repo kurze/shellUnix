4616aceb8476294ceebdd5d7be7b336db371756a
Anais <anais@Linuxanais.(none)>
2013-01-11 16:59:36 +0100

    essai avec le chien de garde

--- shell_fct.c
+++ shell_fct.c
@@ -1,6 +1,13 @@
 #include "shell_fct.h"
 #include <errno.h>
 
+pid_t pid;
+void alarmHandler(int sigNum)
+{
+	printf("Processus fils trop long !\n");
+	kill(pid, SIGKILL);
+}
+
 //prend une commande duement initialisée
 int exec_cmd(cmd * c)
 {
@@ -8,9 +15,9 @@ int exec_cmd(cmd * c)
 //int execvp(const char*file, char*const argv[]);
 //utilise un tableau de pointeurs sur des chaines de caracteres terminées par des caracteres nuls, qui constituent les arguments disponibles pour le programme à exécuter. Le tableau doit se terminer par un pointeur NULL.
 
-	pid_t pid;
 	int ** tube;
 	int i=0,j;
+	int status;
 
 	if (c->nb_membres==0)
 		return -1;
@@ -38,16 +45,17 @@ int exec_cmd(cmd * c)
 		if (i<c->nb_membres)
 			pipe(tube[i-1]);
 		pid = fork();
+		//code du processus fils		
 		if(pid == 0)
 		{
-			//ne passe pas la première fois
+			//ne passe pas la première fois, code non réalisé
 			if (i>1)
 			{
 				close(tube[i-2][1]);
 				dup2(tube[i-2][0], 0);
 				close(tube[i-2][0]);
 			}
-			//ne passe pas la dernière fois
+			//ne passe pas la dernière fois, code non réalisé
 			if (i<c->nb_membres)
 			{
 				close(tube[i-1][0]);
@@ -61,16 +69,24 @@ int exec_cmd(cmd * c)
 			}
 			return 0;
 		}
+		//code du processus pere
+		// préparation du signal d'alame
+		signal(SIGALRM, alarmHandler);
+	
+		// déclenchement de l'alarme
+		alarm(5);
+				
 		if(i>1)
 		{
 			close(tube[i-2][0]);
 			close(tube[i-2][1]);
 		}
 	}
+
 	for (i=1;i<=c->nb_membres;i++)
 		wait(NULL);
-	//desallocation
 
+	//desallocation
 	for(j=0;j<c->nb_membres;j++)
 	{
 		free(tube[j]);
