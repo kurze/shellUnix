b53596d651fb9b137358e654a03ca336707873a5
Simon Kesteloot <kurze@kurze.no-ip.info>
2013-01-15 15:25:25 +0100

    déplacement du client dans un thread à part

--- main.c
+++ main.c
@@ -4,12 +4,25 @@
 #include <pwd.h>
 #include "shell_fct.h"
 #include "constante.h"
+#include <pthread.h>
 
 #include "client.h"
 #include "serveur.h"
 
-int main(void)
-{
-	client();
+int main(int argc, char** argv){
+
+	pthread_t threadClient; /*threadServeur;*/
+	if(argc!=2 || atoi(argv[1])<1 || atoi(argv[1])>65535){
+		printf("./myshell <numéro de port d'écoute>\n\
+				\tnuméro de port compris entre 1024 et 65535\n");
+		exit(1);
+	}
+
+	if(pthread_create(&threadClient, NULL, (void *)client, NULL)){
+		perror("erreur de création de thread\n");
+		exit(1);
+	}
+	pthread_join(threadClient, NULL);
+
 	return 0;
 }
